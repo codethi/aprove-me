8b67dae3dc509f77a990be28593c105e1fc5dd17
codethi <docencia.thiago@gmail.com>
2023-06-27 10:25:12 -0300

    feat[api]: create payble and assingnor

--- receivables-api/src/integrations/dto/create-payble.dto.ts
+++ receivables-api/src/integrations/dto/create-payble.dto.ts
@@ -1,6 +1,8 @@
 import { IsDate, IsDecimal, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreatePaybleDto {
+  id: string;
+
   @IsNotEmpty()
   @IsDecimal({ decimal_digits: '0,2' })
   value: number;
--- receivables-api/src/integrations/integrations.service.ts
+++ receivables-api/src/integrations/integrations.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePaybleDto } from './dto/create-payble.dto';
 import { CreateAssignorDto } from './dto/create-assignor.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class IntegrationsService {
+  constructor(private prisma: PrismaService) {}
+
   createAssignor(createAssignorDto: CreateAssignorDto) {
-    throw new Error('Method not implemented.');
+    return this.prisma.assignor.create({ data: createAssignorDto });
   }
   createPayble(createPaybleDto: CreatePaybleDto) {
-    throw new Error('Method not implemented.');
+    return this.prisma.payble.create({ data: createPaybleDto });
   }
   findOneAssignor(arg0: number) {
     throw new Error('Method not implemented.');
